1a6265426a33505a6aeca3ad9ca060b34aa3e56e
Italoboy <61342502+italoboy@users.noreply.github.com>
2020-11-29 16:46:40 +0330

    Update IP-Info.js

--- IP-Info.js
+++ IP-Info.js
@@ -1,6 +1,11 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: yellow; icon-glyph: magic;
+
+// by italoboy
+
+// Update: 1.
+
  let widget = await createWidget(); Script.setWidget(widget);
 
 async function createWidget() { const req = new Request("http://ipinfo.io/json"); const data = await req.loadJSON(); let IPHeader = ""; let IP = data.ip; let w = new ListWidget(); w.backgroundColor = new Color(color2()); let titleTxt = w.addText(IPHeader);titleTxt.font = new Font("Helvetica",25); titleTxt.textColor = new Color(color1()); let bodyTxt = w.addText(IP);bodyTxt.font = new Font("Helvetica",15); bodyTxt.textColor = new Color(color1());let org = w.addText(data.org);
